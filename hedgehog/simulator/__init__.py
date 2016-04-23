e45b8f9e64610b34eb9a6e45d5e355b6619149e8
Clemens Koza <koza@pria.at>
2016-04-23 20:45:45 +0200

    split HedgehogSimulator into HedgehogServer and SimulatorCommandHandler

--- hedgehog/simulator/__init__.py
+++ hedgehog/simulator/__init__.py
@@ -5,41 +5,7 @@ from hedgehog.protocol import messages, sockets, utils
 from hedgehog.protocol.messages import analog, digital, motor, servo
 
 
-class HedgehogSimulator(threading.Thread):
-    def __init__(self, endpoint, context=None):
-        super(HedgehogSimulator, self).__init__()
-        self.context = context or zmq.Context.instance()
-        self.endpoint = endpoint
-        self.killer = utils.Killer()
-
-    def close(self):
-        self.killer.kill()
-
-    def run(self):
-        socket = self.context.socket(zmq.ROUTER)
-        socket.bind(self.endpoint)
-        socket = sockets.RouterWrapper(socket)
-        killer = self.killer.connect_receiver()
-
-        while True:
-            pollin, _, _ = zmq.select([socket.socket, killer], [], [])
-            if pollin:
-                if socket.socket in pollin:
-                    ident, msg = socket.recv()
-                    try:
-                        handler = getattr(self, msg._command_oneof)
-                    except AttributeError:
-                        # TODO handle unknown commands
-                        print(msg._command_oneof + ': unknown command')
-                        pass
-                    else:
-                        handler(socket, ident, msg)
-                if killer in pollin:
-                    break
-
-        socket.close()
-        killer.close()
-
+class SimulatorCommandHandler:
     def analog_request(self, socket, ident, msg):
         socket.send(ident, messages.analog.Update(msg.port, 0))
 
@@ -76,3 +42,40 @@ class HedgehogSimulator(threading.Thread):
     def servo_state_action(self, socket, ident, msg):
         # TODO set servo active
         pass
+
+
+class HedgehogServer(threading.Thread):
+    def __init__(self, endpoint, handler, context=None):
+        super(HedgehogServer, self).__init__()
+        self.context = context or zmq.Context.instance()
+        self.endpoint = endpoint
+        self.handler = handler
+        self.killer = utils.Killer()
+
+    def close(self):
+        self.killer.kill()
+
+    def run(self):
+        socket = self.context.socket(zmq.ROUTER)
+        socket.bind(self.endpoint)
+        socket = sockets.RouterWrapper(socket)
+        killer = self.killer.connect_receiver()
+
+        while True:
+            pollin, _, _ = zmq.select([socket.socket, killer], [], [])
+            if pollin:
+                if socket.socket in pollin:
+                    ident, msg = socket.recv()
+                    try:
+                        handler = getattr(self.handler, msg._command_oneof)
+                    except AttributeError:
+                        # TODO handle unknown commands
+                        print(msg._command_oneof + ': unknown command')
+                        pass
+                    else:
+                        handler(socket, ident, msg)
+                if killer in pollin:
+                    break
+
+        socket.close()
+        killer.close()
--- tests/test.py
+++ tests/test.py
@@ -2,14 +2,14 @@ import unittest
 import zmq
 from hedgehog.protocol import sockets
 from hedgehog.protocol.messages import analog, digital, motor, servo
-from hedgehog.simulator import HedgehogSimulator
+from hedgehog.simulator import HedgehogServer, SimulatorCommandHandler
 
 
 class TestSimulator(unittest.TestCase):
     def test_analog_request(self):
         context = zmq.Context()
 
-        controller = HedgehogSimulator('tcp://*:5555', context=context)
+        controller = HedgehogServer('tcp://*:5555', SimulatorCommandHandler(), context=context)
         controller.start()
 
         socket = context.socket(zmq.DEALER)
@@ -26,7 +26,7 @@ class TestSimulator(unittest.TestCase):
     def test_digital_request(self):
         context = zmq.Context()
 
-        controller = HedgehogSimulator('tcp://*:5555', context=context)
+        controller = HedgehogServer('tcp://*:5555', SimulatorCommandHandler(), context=context)
         controller.start()
 
         socket = context.socket(zmq.DEALER)
@@ -43,7 +43,7 @@ class TestSimulator(unittest.TestCase):
     def test_motor_request(self):
         context = zmq.Context()
 
-        controller = HedgehogSimulator('tcp://*:5555', context=context)
+        controller = HedgehogServer('tcp://*:5555', SimulatorCommandHandler(), context=context)
         controller.start()
 
         socket = context.socket(zmq.DEALER)
