0d7b887ff6c439e4f8eca62e066894d40c1122d3
Clemens Koza <koza@pria.at>
2016-05-04 13:15:45 +0200

    introduce CommandHandler class

---
+++ hedgehog/server/handlers/__init__.py
@@ -0,0 +1,29 @@
+def command_handlers():
+    _handlers = {}
+
+    def command(msg):
+        def decorator(func):
+            _handlers[msg._command_oneof] = func
+            return func
+        return decorator
+    return _handlers, command
+
+
+def to_dict(*handlers):
+    result = {}
+    for handler in handlers:
+        dups = result.keys() & handler.handlers.keys()
+        if len(dups) > 0:
+            raise ValueError("Duplicate command handler for {}".format(dups))
+        result.update(handler.handlers)
+    return result
+
+
+class CommandHandler:
+    _handlers = None
+
+    def __init__(self):
+        self.handlers = {
+            key: handler.__get__(self, type(self))
+            for key, handler in self._handlers.items()
+        }
--- hedgehog/server/simulator.py
+++ hedgehog/server/simulator.py
@@ -1,48 +1,63 @@
 from hedgehog.protocol.messages import analog, digital, motor, servo, process
 from hedgehog.server.process import run
+from hedgehog.server.handlers import CommandHandler, command_handlers, to_dict
 
 
-class SimulatorCommandHandler:
+class SimulatorHandler(CommandHandler):
+    _handlers, _command = command_handlers()
+
     def __init__(self):
+        super().__init__()
         self._processes = {}
 
+    @_command(analog.Request)
     def analog_request(self, server, ident, msg):
         server.socket.send(ident, analog.Update(msg.port, 0))
 
+    @_command(analog.StateAction)
     def analog_state_action(self, server, ident, msg):
         # TODO set analog pullup
         pass
 
+    @_command(digital.Request)
     def digital_request(self, server, ident, msg):
         server.socket.send(ident, digital.Update(msg.port, False))
 
+    @_command(digital.StateAction)
     def digital_state_action(self, server, ident, msg):
         # TODO set digital pullup, output
         pass
 
+    @_command(digital.Action)
     def digital_action(self, server, ident, msg):
         # TODO set digital pullup, output
         pass
 
+    @_command(motor.Action)
     def motor_action(self, server, ident, msg):
         # TODO set motor action
         pass
 
+    @_command(motor.Request)
     def motor_request(self, server, ident, msg):
         server.socket.send(ident, motor.Update(msg.port, 0, 0))
 
+    @_command(motor.SetPositionAction)
     def motor_set_position_action(self, server, ident, msg):
         # TODO set motor position
         pass
 
+    @_command(servo.Action)
     def servo_action(self, server, ident, msg):
         # TODO set servo position
         pass
 
+    @_command(servo.StateAction)
     def servo_state_action(self, server, ident, msg):
         # TODO set servo active
         pass
 
+    @_command(process.ExecuteRequest)
     def process_execute_request(self, server, ident, msg):
         proc, stdin, stdout, stderr, exit = run(*msg.args)
         pid = proc.pid
@@ -68,14 +83,14 @@ class SimulatorCommandHandler:
         server.register(exit, exit_cb(exit))
         server.socket.send(ident, process.ExecuteReply(pid))
 
+    @_command(process.StreamAction)
     def process_stream_action(self, server, ident, msg):
         _, sockets, _ = self._processes[msg.pid]
         sockets[msg.fileno].send(msg.chunk)
 
 
 def handlers():
-    import inspect
-    return dict(inspect.getmembers(SimulatorCommandHandler(), predicate=inspect.ismethod))
+    return to_dict(SimulatorHandler())
 
 
 def main():
