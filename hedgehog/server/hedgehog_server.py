c68f6223e7d1d47665793ef70f4933d383f7fea5
SillyFreak <sillyfreak.ck@gmail.com>
2017-12-24 15:53:35 +0100

    Implement register properly and use it for handling client commands; remove unregister, instead registered streams should terminate

--- hedgehog/server/hedgehog_server.py
+++ hedgehog/server/hedgehog_server.py
@@ -1,10 +1,10 @@
-from typing import Any, Callable, Coroutine, Dict, Type
+from typing import Any, AsyncGenerator, Callable, Coroutine, Dict, Type
 
 import asyncio
 import logging
 import traceback
 import zmq.asyncio
-from aiostream import pipe
+from aiostream import pipe, streamcontext
 from hedgehog.utils.asyncio import Actor, stream_from_queue
 from hedgehog.utils.zmq.poller import Poller
 from hedgehog.utils.zmq.socket import SocketLike
@@ -27,36 +27,41 @@ class HedgehogServer(Actor):
         self.handlers = handlers
         self.socket = None  # type: DealerRouterSocket
 
-    async def register(self, socket: SocketLike, cb: Callable[[], None]) -> None:
-        await self.cmd_pipe.send((b'REG', socket, cb))
-
-    async def unregister(self, socket: SocketLike) -> None:
-        await self.cmd_pipe.send((b'UNREG', socket))
+    async def register(self, stream: AsyncGenerator[Coroutine[Any, Any, Any], Any]) -> None:
+        await self.cmd_pipe.send((b'REG', stream))
 
     async def send_async(self, ident: Header, *msgs: Message) -> None:
         for msg in msgs:
             logger.debug("Send update:     %s", msg)
         await self.socket.send_msgs(ident, msgs)
 
-    async def _handle(self, ident: Header, msg_raw: RawMessage) -> RawMessage:
-        try:
-            msg = ServerSide.parse(msg_raw)
-            logger.debug("Receive command: %s", msg)
+    async def _requests(self):
+        async def handle_msg(ident, msg_raw):
             try:
-                handler = self.handlers[msg.__class__]
-            except KeyError:
-                raise UnsupportedCommandError(msg.__class__.msg_name())
-            try:
-                result = await handler(self, ident, msg)
-            except HedgehogCommandError:
-                raise
-            except Exception as err:
-                traceback.print_exc()
-                raise FailedCommandError("uncaught exception: {}".format(repr(err))) from err
-        except HedgehogCommandError as err:
-            result = err.to_message()
-        logger.debug("Send reply:      %s", result)
-        return ServerSide.serialize(result)
+                msg = ServerSide.parse(msg_raw)
+                logger.debug("Receive command: %s", msg)
+                try:
+                    handler = self.handlers[msg.__class__]
+                except KeyError:
+                    raise UnsupportedCommandError(msg.__class__.msg_name())
+                try:
+                    result = await handler(self, ident, msg)
+                except HedgehogCommandError:
+                    raise
+                except Exception as err:
+                    traceback.print_exc()
+                    raise FailedCommandError("uncaught exception: {}".format(repr(err))) from err
+            except HedgehogCommandError as err:
+                result = err.to_message()
+            logger.debug("Send reply:      %s", result)
+            return ServerSide.serialize(result)
+
+        async def request_handler(ident, msgs_raw):
+            await self.socket.send_msgs_raw(ident, [await handle_msg(ident, msg) for msg in msgs_raw])
+
+        while True:
+            ident, msgs_raw = await self.socket.recv_msgs_raw()
+            yield request_handler(ident, msgs_raw)
 
     async def run(self, cmd_pipe, evt_pipe):
         self.socket = DealerRouterSocket(self.ctx, zmq.ROUTER, side=ServerSide)
@@ -70,23 +75,19 @@ class HedgehogServer(Actor):
                 cmd = await cmd_pipe.recv()
                 yield (cmd,) if isinstance(cmd, bytes) else cmd
 
-        async def requests():
-            while True:
-                ident, msgs_raw = await self.socket.recv_msgs_raw()
-                yield (b'MSG', ident, msgs_raw)
-
         await stream_queue.put(commands())
-        await stream_queue.put(requests())
+        await self.register(self._requests())
 
         events = stream_from_queue(stream_queue) | pipe.flatten()
         async with events.stream() as streamer:
             async for cmd, *payload in streamer:
-                if cmd == b'$TERM':
+                if cmd == b'EVENT':
+                    awaitable, = payload
+                    await awaitable
+                elif cmd == b'REG':
+                    stream, = payload
+                    await stream_queue.put(streamcontext(stream) | pipe.map(lambda item: (b'EVENT', item)))
+                elif cmd == b'$TERM':
                     break
-                elif cmd in {b'REG', b'UNREG'}:
-                    raise NotImplementedError()
-                elif cmd == b'MSG':
-                    ident, msgs_raw = payload
-                    await self.socket.send_msgs_raw(ident, [await self._handle(ident, msg) for msg in msgs_raw])
 
         self.socket.close()
