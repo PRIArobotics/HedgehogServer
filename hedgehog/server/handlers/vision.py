51b4df85163fe28aded610e3554e636b54bc5003
Clemens Koza <koza@pria.at>
2019-09-15 10:52:42 +0200

    getting frames from the camera works

--- hedgehog/server/handlers/vision.py
+++ hedgehog/server/handlers/vision.py
@@ -1,15 +1,48 @@
+from contextlib import AsyncExitStack
+
+import trio
+
+from hedgehog.protocol.errors import FailedCommandError
 from hedgehog.protocol.messages import ack, vision
 
 from . import CommandHandler, CommandRegistry
+from ..vision import open_camera, Grabber
 
 
 class VisionHandler(CommandHandler):
     _commands = CommandRegistry()
 
+    def __init__(self) -> None:
+        super().__init__()
+        self.open_count = 0
+        self._stack = None  # AsyncExitStack
+        self._grabber = None  # Grabber
+
     @_commands.register(vision.CameraAction)
     async def camera_action(self, server, ident, msg):
+        if msg.open:
+            if self.open_count == 0:
+                self._stack = AsyncExitStack()
+                cam = await trio.to_thread.run_sync(open_camera)
+                self._grabber = Grabber(cam)
+                await self._stack.enter_async_context(self._grabber)
+            self.open_count += 1
+        else:
+            if self.open_count == 0:
+                raise FailedCommandError("trying to close already closed camera")
+
+            self.open_count -= 1
+            if self.open_count == 0:
+                await self._stack.aclose()
+                self._grabber = None
+                self._stack = None
+
         return ack.Acknowledgement()
 
     @_commands.register(vision.ReadFrameAction)
     async def read_frame_action(self, server, ident, msg):
+        s, im = await self._grabber.aread()
+        if not s:
+            raise FailedCommandError("camera did not return an image")
+
         return ack.Acknowledgement()
---
+++ hedgehog/server/vision/__init__.py
@@ -0,0 +1,85 @@
+import logging
+import cv2
+import trio
+
+from threading import Condition, Thread
+
+logger = logging.getLogger(__name__)
+
+
+class Grabber:
+    """
+    Grabber ensures that camera frames are grabbed as soon as they arrive, so that the latest frame is always processed.
+    To do this, Grabber starts a background thread for reading the camera.
+    Grabber manages that thread as a context manager.
+    Only when the application wants to process a frame, that frame is retrieved using the `read` method.
+    `read` will always return the latest data fetched from the capture device,
+    even if that data indicated there are no frames left.
+
+    Grabber offers an async interface for trio as well, with an async context manager and the `aread` method.
+    """
+
+
+    _EMPTY = 0
+    _GRABBED = 1
+    _CLOSED = 2
+
+    def __init__(self, cap):
+        self._cap = cap
+        self._state = Grabber._EMPTY
+        self._cond = Condition()
+
+    def __enter__(self):
+        Thread(target=self._grabber).start()
+        return self
+
+    async def __aenter__(self):
+        self.__enter__()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        with self._cond:
+            self._state = Grabber._CLOSED
+
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        await trio.to_thread.run_sync(self.__exit__, exc_type, exc_value, traceback)
+
+    def _grabber(self):
+        logger.info("Grabber thread started")
+        while True:
+            with self._cond:
+                if self._state == Grabber._CLOSED:
+                    break
+                elif self._cap.grab():
+                    # set a flag that a frame may be retrieved
+                    self._state = Grabber._GRABBED
+                    self._cond.notify_all()
+                else:
+                    # set a flag that EOF was reached
+                    # grab() has probably invalidated the previous frame
+                    # that's ok; it's old by now and we don't want it
+                    self._state = Grabber._CLOSED
+                    self._cond.notify_all()
+                    break
+        self._cap.release()
+        logger.info("Grabber thread finished")
+
+    def read(self):
+        with self._cond:
+            self._cond.wait_for(lambda: self._state != Grabber._EMPTY)
+            if self._state == Grabber._GRABBED:
+                # decode and retrieve the latest grabbed frame
+                self._state = Grabber._EMPTY
+                return self._cap.retrieve()
+            else:
+                # grabber was closed
+                return False, None
+
+    async def aread(self):
+        return await trio.to_thread.run_sync(self.read)
+
+
+def open_camera():
+    cam = cv2.VideoCapture(0)
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, 160)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 120)
+    return cam
