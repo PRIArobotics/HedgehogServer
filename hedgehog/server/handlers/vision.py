038e4fd98f9b3265681914500991805e7843d77f
Clemens Koza <koza@pria.at>
2019-09-19 12:39:50 +0200

    add channel handling

--- hedgehog/server/handlers/vision.py
+++ hedgehog/server/handlers/vision.py
@@ -1,7 +1,13 @@
+from typing import Callable, Dict, DefaultDict, Generic, Optional, TypeVar
+
+from collections import defaultdict
 from contextlib import AsyncExitStack
+from dataclasses import dataclass
+from functools import partial
 
 import trio
 import cv2
+import numpy as np
 
 from hedgehog.protocol.errors import FailedCommandError
 from hedgehog.protocol.messages import ack, vision as vision_msg
@@ -10,15 +16,43 @@ from . import CommandHandler, CommandRegistry
 from .. import vision
 
 
+T = TypeVar('T')
+
+@dataclass
+class Channel(Generic[T]):
+    msg: vision_msg.Channel
+    detect: Callable[[np.ndarray], T]
+    highlight: Callable[[np.ndarray, T], np.ndarray]
+
+
+class ChannelData(Generic[T]):
+    result: Optional[T] = None
+    highlight: Optional[np.ndarray] = None
+
+    def get_result(self, img: np.ndarray, channel: Channel[T]):
+        if self.result is None:
+            self.result = channel.detect(img)
+        return self.result
+
+    def get_highlight(self, img: np.ndarray, channel: Channel[T]):
+        if self.highlight is None:
+            result = self.get_result(img, channel)
+            self.highlight = channel.highlight(img, result)
+        return self.highlight
+
+
 class VisionHandler(CommandHandler):
     _commands = CommandRegistry()
 
+    open_count: int = 0
+    _stack: Optional[AsyncExitStack] = None
+    _grabber: Optional[vision.Grabber] = None
+    _channels: Dict[str, Channel]
+    _img: Optional[np.ndarray] = None
+    _channel_data: Optional[DefaultDict[str, ChannelData]] = None
+
     def __init__(self) -> None:
-        super().__init__()
-        self.open_count = 0
-        self._stack = None  # AsyncExitStack
-        self._grabber = None  # Grabber
-        self._img = None
+        self._channels = {}
 
     @_commands.register(vision_msg.OpenCameraAction)
     async def open_camera_action(self, server, ident, msg):
@@ -42,8 +76,65 @@ class VisionHandler(CommandHandler):
             self._grabber = None
             self._stack = None
             self._img = None
+            self._channel_data = None
 
         return ack.Acknowledgement()
+
+    def set_channels(self, channels: Dict[str, vision_msg.Channel]):
+        for key, channel in channels.items():
+            if isinstance(channel, vision_msg.FacesChannel):
+                self._channels[key] = Channel(
+                    channel,
+                    partial(vision.detect_faces, vision.haar_face_cascade),
+                    vision.highlight_faces
+                )
+            elif isinstance(channel, vision_msg.ContoursChannel):
+                self._channels[key] = Channel(
+                    channel,
+                    partial(vision.detect_contours, min_hsv=channel.hsv_min, max_hsv=channel.hsv_max),
+                    vision.highlight_contours
+                )
+            else:  # pragma: nocover
+                assert False
+
+    @_commands.register(vision_msg.CreateChannelAction)
+    async def create_channel_action(self, server, ident, msg):
+        dups = set(msg.channels) & set(self._channels)
+        if dups:
+            raise FailedCommandError(f"duplicate keys: {', '.join(dups)}")
+
+        self.set_channels(msg.channels)
+        return ack.Acknowledgement()
+
+    @_commands.register(vision_msg.UpdateChannelAction)
+    async def update_channel_action(self, server, ident, msg):
+        missing = set(msg.channels) - set(self._channels)
+        if missing:
+            raise FailedCommandError(f"nonexistent keys: {', '.join(missing)}")
+
+        self.set_channels(msg.channels)
+        return ack.Acknowledgement()
+
+    @_commands.register(vision_msg.DeleteChannelAction)
+    async def delete_channel_action(self, server, ident, msg):
+        missing = msg.keys - set(self._channels)
+        if missing:
+            raise FailedCommandError(f"nonexistent keys: {', '.join(missing)}")
+
+        for key in msg.keys:
+            del self._channels[key]
+        return ack.Acknowledgement()
+
+    @_commands.register(vision_msg.ChannelRequest)
+    async def channel_request(self, server, ident, msg):
+        missing = msg.keys - set(self._channels)
+        if missing:
+            raise FailedCommandError(f"nonexistent keys: {', '.join(missing)}")
+
+        if msg.keys:
+            return vision_msg.ChannelReply({key: self._channels[key].msg for key in msg.keys})
+        else:
+            return vision_msg.ChannelReply({key: channel.msg for key, channel in self._channels.items()})
 
     @_commands.register(vision_msg.CaptureFrameAction)
     async def capture_frame_action(self, server, ident, msg):
@@ -53,9 +144,11 @@ class VisionHandler(CommandHandler):
         s, img = await self._grabber.aread()
         if not s:
             self._img = None
+            self._channel_data = None
             raise FailedCommandError("camera did not return an image")
 
         self._img = img
+        self._channel_data = defaultdict(ChannelData)
 
         return ack.Acknowledgement()
 
@@ -64,7 +157,15 @@ class VisionHandler(CommandHandler):
         if self._img is None:
             raise FailedCommandError("no frame available")
 
-        s, img = cv2.imencode('.jpg', self._img)
+        if msg.highlight is None:
+            img = self._img
+        elif msg.highlight in self._channels:
+            channel_data = self._channel_data[msg.highlight]
+            img = channel_data.get_highlight(self._img, self._channels[msg.highlight])
+        else:
+            raise FailedCommandError(f"no such channel: {msg.highlight}")
+
+        s, img = cv2.imencode('.jpg', img)
         if not s:
             raise FailedCommandError("encoding image failed")
 
